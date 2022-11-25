f2d3e5f94c53ba857bb4fcfc2f21b6597f6298b9
dcarpencu <dcarpencu@gmail.com>
2022-11-25 21:54:01 +0200

    Added marker for maps

--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -1,21 +1,18 @@
 import React, {useState, useEffect} from "react";
 import GoogleMapReact from 'google-map-react';
 import GoogleMap from 'google-map-react';
+import {Marker} from "./Marker";
 
 export const SimpleMap = () => {
   const [currentLocation, setCurrentLocation] = useState();
 
   const success = (pos) => {
-    const crd = pos.coords;
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
+    setCurrentLocation(pos.coords);
+    console.log(currentLocation);
   }
 
-
   useEffect(() => {
-    setCurrentLocation(navigator.geolocation.getCurrentPosition(success));
+    navigator.geolocation.getCurrentPosition(success);
   }, [])
   
   const defaultProps = {
@@ -23,14 +20,15 @@ export const SimpleMap = () => {
     zoom: 9,
     greatPlaceCoords: {lat: 45.760696, lng: 21.226788}
   };
-
+  // console.log(currentLocation);
   
   return (
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMap
-          bootstrapURLKeys={{key: "AIzaSyCX7DVZTuz23vmFeYrdhw55kD-j_d8U_uo"}} // set if you need stats etc ...
+          bootstrapURLKeys={{key: "AIzaSyCX7DVZTuz23vmFeYrdhw55kD-j_d8U_uo"}}
           center={[45.760696, 21.226788]}
           zoom={9}>
+        <Marker lat={45.760696} lng={21.226788}/>
       </GoogleMap>
     </div>
   );
