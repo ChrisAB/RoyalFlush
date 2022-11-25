d370c61b5a4e36223714d6455ee34e7979c87826
dcarpencu <dcarpencu@gmail.com>
2022-11-25 21:54:01 +0200

    Added marker for maps

---
+++ client/src/Components/Map/Marker.jsx
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from "react";
+import {greatPlaceStyle} from "./Marker-styles";
+
+export const Marker = () => {
+    return <div style={greatPlaceStyle}>
+        abc
+    </div>
+}
